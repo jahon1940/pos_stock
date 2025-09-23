72b6e89b9a55acc1e59195396b0d659545148298
alpjamal <mr.abdujamol1@mail.ru>
2025-09-23 15:36:55 +0500

    extend StateStatus enum with getters

--- lib/core/enums/states.dart
+++ lib/core/enums/states.dart
@@ -1,1 +1,20 @@
-enum StateStatus { initial, loading, loaded, error, success, loadingMore }
+enum StateStatus {
+  initial,
+  loading,
+  loaded,
+  error,
+  success,
+  loadingMore;
+
+  bool get isInitial => this == StateStatus.initial;
+
+  bool get isLoading => this == StateStatus.loading;
+
+  bool get isLoaded => this == StateStatus.loaded;
+
+  bool get isError => this == StateStatus.error;
+
+  bool get isSuccess => this == StateStatus.success;
+
+  bool get isLoadingMore => this == StateStatus.loadingMore;
+}
