eebf107e4c40a92c1447575331122c77446a4804
alpjamal <mr.abdujamol1@mail.ru>
2025-10-03 17:24:55 +0500

    delete write off related methods

--- lib/data/sources/network/stock_api/stock_api.dart
+++ lib/data/sources/network/stock_api/stock_api.dart
@@ -1,4 +1,3 @@
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:hoomo_pos/core/constants/network.dart';
 import 'package:hoomo_pos/core/network/dio_client.dart';
@@ -11,23 +10,11 @@ import 'package:injectable/injectable.dart';
 import '../../../dtos/stock_dto.dart';
 import '../../../dtos/supplies_1c/supplies_1c.dart';
 import '../../../dtos/supplies_1c/supplies_1c_conduct.dart';
-import '../../../dtos/write_offs/create_write_off.dart';
-import '../../../dtos/write_offs/search_write_off.dart';
-import '../../../dtos/write_offs/write_off_dto.dart';
-import '../../../dtos/write_offs/write_off_product_dto.dart';
 
 part 'stock_api_impl.dart';
 
 abstract class StockApi {
   Future<List<StockDto>?> getStocks(int organizationsId);
-
-  Future<PaginatedDto<WriteOffDto>> searchWriteOff(SearchWriteOff request);
-
-  Future<void> createWriteOff(CreateWriteOff request);
-
-  Future<List<WriteOffProductDto>> getWriteOffProducts(int id);
-
-  Future<void> deleteWriteOff(int id);
 
   Future<List<SupplyDto>?> getSupplies();
 
@@ -36,8 +23,4 @@ abstract class StockApi {
   Future<List<SupplyProductDto>> getSupply1CProducts(int id);
 
   Future<void> conductSupplies1C(SuppliesConduct request);
-
-  Future<void> downloadWriteOffs({
-    required int id,
-  });
 }
