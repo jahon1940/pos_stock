f80ade14cf8def296892c3bc212e1aa3b177af4c
alpjamal <mr.abdujamol1@mail.ru>
2025-10-03 17:26:04 +0500

    remove unused import

--- lib/data/dtos/write_offs/create_write_off.dart
+++ lib/data/dtos/write_offs/create_write_off.dart
@@ -1,8 +1,8 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:hoomo_pos/data/dtos/add_product/add_product_request.dart';
 import 'package:hoomo_pos/data/dtos/write_offs/write_off_product_request.dart';
 
 part 'create_write_off.freezed.dart';
+
 part 'create_write_off.g.dart';
 
 @Freezed(fromJson: false, toJson: true)
