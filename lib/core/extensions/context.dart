c9d6314ef0ecb1ee945ee4607be6cc28770f95bc
alpjamal <mr.abdujamol1@mail.ru>
2025-10-03 16:58:38 +0500

    WriteOffCubit extension added

--- lib/core/extensions/context.dart
+++ lib/core/extensions/context.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hoomo_pos/presentation/desktop/screens/stock/screens/inventories/cubit/inventory_cubit.dart';
 import 'package:hoomo_pos/presentation/desktop/screens/stock/screens/supplies/cubit/supply_cubit.dart';
 import 'package:hoomo_pos/presentation/desktop/screens/stock/screens/transfer/cubit/transfer_cubit.dart';
+import 'package:hoomo_pos/presentation/desktop/screens/stock/screens/write_off/cubit/write_off_cubit.dart';
 import '../../presentation/desktop/dialogs/category/bloc/category_bloc.dart';
 import '../../presentation/desktop/screens/manager/children/cubit/manager_cubit.dart';
 import '../../presentation/desktop/screens/reports/children/cubit/reports_cubit.dart';
@@ -190,4 +191,6 @@ extension BlocExtension on BuildContext {
   SupplyCubit get supplyBloc => read<SupplyCubit>();
 
   TransferCubit get transferBloc => read<TransferCubit>();
+
+  WriteOffCubit get writeOffBloc => read<WriteOffCubit>();
 }
