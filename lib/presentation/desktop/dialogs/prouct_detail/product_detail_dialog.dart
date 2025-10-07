8c5dacc1c76b47c4d4e5929243f76279f7927002
alpjamal <mr.abdujamol1@mail.ru>
2025-10-07 16:58:46 +0500

    refactoring

--- lib/presentation/desktop/dialogs/prouct_detail/product_detail_dialog.dart
+++ lib/presentation/desktop/dialogs/prouct_detail/product_detail_dialog.dart
@@ -19,189 +19,176 @@ class ProductDetailDialog extends HookWidget {
   final ProductDto productDto;
 
   @override
-  Widget build(BuildContext context) {
-    ThemeData themeData = Theme.of(context);
-
+  Widget build(
+    BuildContext context,
+  ) {
     useEffect(() {
       context.read<ProductDetailCubit>().search(productDto.id);
       return null;
     }, []);
-
     return SelectionArea(
       child: DefaultTabController(
-        initialIndex: 0,
         length: 3,
         child: Padding(
           padding: const EdgeInsets.fromLTRB(80, 15, 80, 15),
           child: Material(
             borderRadius: BorderRadius.circular(12),
             child: BlocBuilder<ProductDetailCubit, ProductDetailState>(
-                builder: (context, state) {
-              if (state.status == StateStatus.loading) {
-                return Center(child: CircularProgressIndicator());
-              } else if (state.status == StateStatus.loaded) {
-                return Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              flex: 10,
-                              child: Text(productDto.title ?? "",
-                                  style: AppTextStyles.boldType16),
-                            ),
-                            Row(
-                              spacing: 10,
-                              children: [
-                                DecoratedBox(
-                                  decoration: BoxDecoration(
-                                    color: AppColors.success500,
-                                    borderRadius: BorderRadius.circular(6),
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(5),
-                                    child: TextButton(
-                                      onPressed: () {
-                                        context
-                                            .read<ProductDetailCubit>()
-                                            .syncProduct(productDto.id);
-                                      },
-                                      child: Text(
-                                        "Синхронизировать",
-                                        style: AppTextStyles.boldType16
-                                            .copyWith(color: AppColors.white),
+              builder: (context, state) {
+                if (state.status == StateStatus.loading) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (state.status == StateStatus.loaded) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                flex: 10,
+                                child: Text(productDto.title ?? '', style: AppTextStyles.boldType16),
+                              ),
+                              Row(
+                                spacing: 10,
+                                children: [
+                                  DecoratedBox(
+                                    decoration: BoxDecoration(
+                                      color: AppColors.success500,
+                                      borderRadius: BorderRadius.circular(6),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(5),
+                                      child: TextButton(
+                                        onPressed: () {
+                                          context.read<ProductDetailCubit>().syncProduct(productDto.id);
+                                        },
+                                        child: Text(
+                                          'Синхронизировать',
+                                          style: AppTextStyles.boldType16.copyWith(color: AppColors.white),
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
-                                SizedBox(
-                                  height: 50,
-                                  width: 50,
-                                  child: DecoratedBox(
-                                    decoration: BoxDecoration(
-                                      color: AppColors.error100,
-                                      borderRadius: BorderRadius.circular(6),
-                                    ),
-                                    child: IconButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      style: IconButton.styleFrom(
-                                          overlayColor: AppColors.error500),
-                                      icon: Icon(Icons.close,
-                                          color: AppColors.error600),
+                                  SizedBox(
+                                    height: 50,
+                                    width: 50,
+                                    child: DecoratedBox(
+                                      decoration: BoxDecoration(
+                                        color: AppColors.error100,
+                                        borderRadius: BorderRadius.circular(6),
+                                      ),
+                                      child: IconButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        style: IconButton.styleFrom(overlayColor: AppColors.error500),
+                                        icon: const Icon(Icons.close, color: AppColors.error600),
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
-                            )
-                          ],
+                                ],
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                      AppSpace.vertical12,
-                      SizedBox(
-                        width: context.width * .7,
-                        child: TabBar(
-                          overlayColor: MaterialStateProperty.all<Color>(
-                              Colors.transparent),
-                          dividerColor: Colors.transparent,
-                          labelColor: Colors.white,
-                          unselectedLabelColor: Colors.grey,
-                          indicator: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: context.primary),
-                          tabs: <Widget>[
-                            // for (final index in [0, 1, 2])
-                            SizedBox(
-                              width: context.width * .2,
-                              child: Tab(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    "Информация о Наменклатуре",
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
+                        AppSpace.vertical12,
+                        SizedBox(
+                          width: context.width * .7,
+                          child: TabBar(
+                            overlayColor: WidgetStateProperty.all<Color>(Colors.transparent),
+                            dividerColor: Colors.transparent,
+                            labelColor: Colors.white,
+                            unselectedLabelColor: Colors.grey,
+                            indicator: BoxDecoration(borderRadius: BorderRadius.circular(10), color: context.primary),
+                            tabs: <Widget>[
+                              // for (final index in [0, 1, 2])
+                              SizedBox(
+                                width: context.width * .2,
+                                child: const Tab(
+                                  child: Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: Text(
+                                      'Информация о Наменклатуре',
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            SizedBox(
-                              width: context.width * .2,
-                              child: Tab(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    context.tr("availability_in_stock"),
+                              SizedBox(
+                                width: context.width * .2,
+                                child: Tab(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      context.tr('availability_in_stock'),
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            SizedBox(
-                              width: context.width * .2,
-                              child: Tab(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    "Штрихкоды",
+                              SizedBox(
+                                width: context.width * .2,
+                                child: const Tab(
+                                  child: Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: Text(
+                                      'Штрихкоды',
+                                    ),
                                   ),
                                 ),
                               ),
+                            ],
+                          ),
+                        ),
+                        AppSpace.vertical6,
+                        const Divider(),
+                        AppSpace.vertical6,
+                        SizedBox(
+                          height: context.height * .65,
+                          child: TabBarView(
+                            children: <Widget>[
+                              ProductInfo(productDto),
+                              QuantityInfo(productDto),
+                              UpdateBarcode(productDto),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+                return Center(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Ошибка загрузки'),
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: AppColors.success500,
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: TextButton(
+                            onPressed: () {
+                              context.read<ProductDetailCubit>().search(productDto.id);
+                            },
+                            child: Text(
+                              'Обновить',
+                              style: AppTextStyles.boldType16.copyWith(color: AppColors.white),
                             ),
-                          ],
-                        ),
-                      ),
-                      AppSpace.vertical6,
-                      Divider(),
-                      AppSpace.vertical6,
-                      SizedBox(
-                        height: context.height * .65,
-                        child: TabBarView(
-                          children: <Widget>[
-                            ProductInfo(productDto),
-                            QuantityInfo(productDto),
-                            UpdateBarcode(productDto),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              }
-              return Center(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Ошибка загрузки"),
-                  Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                        color: AppColors.success500,
-                        borderRadius: BorderRadius.circular(6),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(5),
-                        child: TextButton(
-                          onPressed: () {
-                            context
-                                .read<ProductDetailCubit>()
-                                .search(productDto.id);
-                          },
-                          child: Text(
-                            "Обновить",
-                            style: AppTextStyles.boldType16
-                                .copyWith(color: AppColors.white),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ));
-            }),
+                  ],
+                ));
+              },
+            ),
           ),
         ),
       ),
