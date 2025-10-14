9991edec9332b8966efa0bd6b236e0eef42c16fc
alpjamal <mr.abdujamol1@mail.ru>
2025-10-14 20:40:12 +0500

    pick image feature

--- lib/presentation/desktop/screens/stock/screens/stock_products/widgets/product_images_widget.dart
+++ lib/presentation/desktop/screens/stock/screens/stock_products/widgets/product_images_widget.dart
@@ -1,150 +1,179 @@
+import 'dart:io' show File;
+import 'dart:math' show min;
+
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hoomo_pos/core/extensions/context.dart';
+import 'package:hoomo_pos/core/extensions/null_extension.dart';
 import 'package:hoomo_pos/core/extensions/text_style_extension.dart';
 import 'package:hoomo_pos/core/styles/text_style.dart';
 import 'package:hoomo_pos/core/widgets/custom_box.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../../../../../../core/constants/app_utils.dart';
 
-class ProductImagesWidget extends HookWidget {
+class ProductImagesWidget extends StatefulWidget {
   const ProductImagesWidget({
     super.key,
   });
 
   @override
-  Widget build(
-    BuildContext context,
-  ) {
-    // int drag = 1;
-    // final oneCTitleController = useTextEditingController();
-    // final barcodeController = useTextEditingController();
-    // final codeController = useTextEditingController();
-    // final quantityController = useTextEditingController();
-    // List<TextEditingController> charactersDescriptionController = [TextEditingController()];
-    // List<TextEditingController> charactersController = [TextEditingController()];
-    return CustomBox(
-      padding: AppUtils.kPaddingAll12,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          AppUtils.kGap16,
-          Text(
-            'Фото продукта',
-            style: AppTextStyles.boldType14.copyWith(fontWeight: FontWeight.w500, height: 1),
-          ),
-          AppUtils.kGap20,
-          const _EmptyImageWidget(),
-        ],
-      ),
-    );
-  }
+  State<ProductImagesWidget> createState() => _ProductImagesWidgetState();
 }
 
-class _EmptyImageWidget extends StatelessWidget {
-  const _EmptyImageWidget();
+class _ProductImagesWidgetState extends State<ProductImagesWidget> {
+  final List<File> _images = [];
+  File? _selectedItem;
+
+  Future<void> _pickImage() async {
+    try {
+      final xfile = await ImagePicker().pickMedia();
+      if (xfile.isNotNull) {
+        final file = File(xfile!.path);
+        for (final item in _images) {
+          if (item.path == file.path) return;
+        }
+        if (_images.isEmpty) _selectedItem = file;
+        _images.add(file);
+        setState(() {});
+      }
+    } catch (e) {
+      debugPrint('asklfjdasdf image pick error $e');
+    }
+  }
 
   @override
   Widget build(
     BuildContext context,
   ) =>
-      Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(
-            width: 314,
-            height: 314,
-            decoration: BoxDecoration(
-              borderRadius: AppUtils.kBorderRadius12,
-              border: Border.all(width: 2, color: Colors.grey.shade300),
+      CustomBox(
+        padding: AppUtils.kPaddingAll12,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            AppUtils.kGap16,
+            Text(
+              'Фото продукта',
+              style: AppTextStyles.boldType14.copyWith(fontWeight: FontWeight.w500, height: 1),
             ),
-          ),
-          for (int i = 0; i < 30; i++)
-            Positioned(
-              top: i * 13 + 5,
-              child: Container(
-                width: 314,
-                height: 5,
-                color: context.cardColor,
+            AppUtils.kGap20,
+
+            /// image or pick image button
+            if (_selectedItem.isNotNull)
+              ClipRRect(
+                borderRadius: AppUtils.kBorderRadius12,
+                child: Container(
+                  color: Colors.grey,
+                  width: 316,
+                  height: 316,
+                  child: Image.file(
+                    _selectedItem!,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              )
+            else
+              _EmptyImageWidget(
+                onTap: _pickImage,
               ),
-            ),
-          for (int i = 0; i < 30; i++)
-            Positioned(
-              left: i * 13 + 5,
-              child: Container(
-                width: 5,
-                height: 314,
-                color: context.cardColor,
-              ),
-            ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.image_outlined,
-                color: context.theme.disabledColor,
-                size: 30,
-              ),
-              AppUtils.kGap6,
-              Text(
-                'Upload your product image.',
-                style: AppTextStyles.rType16.copyWith(
-                  fontWeight: FontWeight.w500,
-                  color: context.theme.unselectedWidgetColor,
+
+            /// mini images
+            if (_images.isNotEmpty) ...[
+              AppUtils.kGap12,
+              Row(
+                spacing: AppUtils.kGap12.mainAxisExtent,
+                children: List.generate(
+                  min(4, _images.length + 1),
+                  (index) {
+                    final isLast = index == _images.length;
+                    final file = isLast ? null : _images.elementAt(index);
+                    final isSelected = file.isNotNull && file?.path == _selectedItem?.path;
+                    return isLast
+                        ? OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                              shape: const RoundedRectangleBorder(borderRadius: AppUtils.kBorderRadius12),
+                              fixedSize: const Size(70, 70),
+                              side: BorderSide(color: Colors.grey.shade300),
+                              overlayColor: Colors.grey,
+                              padding: EdgeInsets.zero,
+                            ),
+                            onPressed: _pickImage,
+                            child: Icon(Icons.add_a_photo_outlined, color: Colors.grey.shade300),
+                          )
+                        : GestureDetector(
+                            onTap: () => setState(() => _selectedItem = file),
+                            child: Container(
+                              height: 70,
+                              width: 70,
+                              decoration: BoxDecoration(
+                                borderRadius: AppUtils.kBorderRadius12,
+                                border: isSelected ? Border.all(color: context.primary, width: 3) : null,
+                              ),
+                              child: ClipRRect(
+                                borderRadius: AppUtils.kBorderRadius8,
+                                child: Image.file(
+                                  file!,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          );
+                  },
                 ),
               ),
-              AppUtils.kGap6,
-              Text(
-                'Only PNG, JPG format allowed.',
-                style: AppTextStyles.rType12.withColor(context.theme.disabledColor),
-              ),
-              AppUtils.kGap6,
-              Text(
-                '500x500 pixels are recommended.',
-                style: AppTextStyles.rType12.withColor(context.theme.disabledColor),
-              ),
             ],
-          ),
-        ],
+          ],
+        ),
       );
 }
 
-// DraggableFile(
-//     size: const Size(314, 314),
-//     onDragDone: viewModel.productImage,
-//     networkImage: viewModel.product.gallery?.first),
-// Wrap(
-//   direction: Axis.horizontal,
-//   spacing: 12,
-//   children: List.generate(drag + 1, (index) {
-//     return Padding(
-//       padding: const EdgeInsets.only(bottom: 10),
-//       child: Stack(
-//         children: [
-//           // DraggableFile(
-//           //   networkImage: index == viewModel.drag
-//           //       ? ""
-//           //       : viewModel.gallery?[index],
-//           //   textOne: '',
-//           //   textTwo: 'Выберите\n     фото',
-//           //   textThree: '',
-//           //   size: const Size(90, 80),
-//           //   onDragDone: viewModel.productImage,
-//           // ),
-//           drag == 0
-//               ? const SizedBox()
-//               : Positioned(
-//                   top: 5,
-//                   right: 5,
-//                   child: CircleAvatar(
-//                     backgroundColor: AppColors.error700,
-//                     radius: 10,
-//                     child: IconButton(
-//                         icon: Icon(Icons.close), onPressed: () {}),
-//                   )),
-//         ],
-//       ),
-//     );
-//   }),
-// )
+class _EmptyImageWidget extends StatelessWidget {
+  const _EmptyImageWidget({
+    required this.onTap,
+  });
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) =>
+      OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          shape: const RoundedRectangleBorder(borderRadius: AppUtils.kBorderRadius12),
+          fixedSize: const Size(316, 316),
+          side: BorderSide(color: Colors.grey.shade300),
+          overlayColor: Colors.grey,
+          padding: EdgeInsets.zero,
+        ),
+        onPressed: onTap,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.image_outlined,
+              color: context.theme.disabledColor,
+              size: 30,
+            ),
+            AppUtils.kGap6,
+            Text(
+              'Upload your product image.',
+              style: AppTextStyles.rType16.copyWith(
+                fontWeight: FontWeight.w500,
+                color: context.theme.unselectedWidgetColor,
+              ),
+            ),
+            AppUtils.kGap6,
+            Text(
+              'Only PNG, JPG format allowed.',
+              style: AppTextStyles.rType12.withColor(context.theme.disabledColor),
+            ),
+            AppUtils.kGap6,
+            Text(
+              '500x500 pixels are recommended.',
+              style: AppTextStyles.rType12.withColor(context.theme.disabledColor),
+            ),
+          ],
+        ),
+      );
+}
