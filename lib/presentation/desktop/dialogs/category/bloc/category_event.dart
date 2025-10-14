12d92282f85c3d3987f6932608db38eb2dfc1360
alpjamal <mr.abdujamol1@mail.ru>
2025-10-14 13:55:05 +0500

    format changes

--- lib/presentation/desktop/dialogs/category/bloc/category_event.dart
+++ lib/presentation/desktop/dialogs/category/bloc/category_event.dart
@@ -5,32 +5,38 @@ abstract class CategoryEvent {}
 class GetCategory extends CategoryEvent {}
 
 class GetCategoryId extends CategoryEvent {
-  final int? id;
   GetCategoryId(this.id);
+
+  final int? id;
 }
 
 class CreateCategory extends CategoryEvent {
-  final CreateCategoryRequest request;
   CreateCategory(this.request);
+
+  final CreateCategoryRequest request;
 }
 
 class PrintPrice extends CategoryEvent {
-  final CreateCategoryRequest request;
   PrintPrice(this.request);
+
+  final CreateCategoryRequest request;
 }
 
 class UpdateCategory extends CategoryEvent {
+  UpdateCategory(this.request, this.id);
+
   final String? id;
   final CreateCategoryRequest request;
-  UpdateCategory(this.request, this.id);
 }
 
 class DeleteCategoryId extends CategoryEvent {
-  final String? id;
   DeleteCategoryId(this.id);
+
+  final String? id;
 }
 
 class SelectedCategory extends CategoryEvent {
-  final int? selectedCategory;
   SelectedCategory(this.selectedCategory);
+
+  final int? selectedCategory;
 }
