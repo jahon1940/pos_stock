b3bdf4389b8f4ac9450983af880b50922b779ba9
alpjamal <mr.abdujamol1@mail.ru>
2025-10-14 17:31:04 +0500

    country dto created

---
+++ lib/data/dtos/country/country_dto.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'country_dto.freezed.dart';
+
+part 'country_dto.g.dart';
+
+@freezed
+class CountryDto with _$CountryDto {
+  factory CountryDto({
+    required int id,
+    String? name,
+    String? cid,
+  }) = _CountryDto;
+
+  factory CountryDto.fromJson(Map<String, dynamic> json) => _$CountryDtoFromJson(json);
+}
