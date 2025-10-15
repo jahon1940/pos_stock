127094844c3f6ab276fdb864c4068e8dc8a19038
alpjamal <mr.abdujamol1@mail.ru>
2025-10-15 16:58:33 +0500

    member type changes

--- lib/presentation/desktop/screens/stock/screens/brands/widgets/create_brand_dialog.dart
+++ lib/presentation/desktop/screens/stock/screens/brands/widgets/create_brand_dialog.dart
@@ -24,138 +24,143 @@ class CreateBrandDialog extends StatefulWidget {
 
 class _CreateBrandDialogState extends State<CreateBrandDialog> {
   File? _imageFile;
+  late final TextEditingController _nameController;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController();
+  }
 
   @override
   Widget build(
     BuildContext context,
-  ) {
-    final nameController = TextEditingController();
-    return AlertDialog(
-      shape: const RoundedRectangleBorder(borderRadius: AppUtils.kBorderRadius12),
-      contentPadding: AppUtils.kPaddingAll24,
-      content: SizedBox(
-        width: context.width * .3,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            /// header
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                /// title
-                const Text(
-                  'Создания бренд',
-                  style: AppTextStyles.boldType18,
-                ),
+  ) =>
+      AlertDialog(
+        shape: const RoundedRectangleBorder(borderRadius: AppUtils.kBorderRadius12),
+        contentPadding: AppUtils.kPaddingAll24,
+        content: SizedBox(
+          width: context.width * .3,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              /// header
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  /// title
+                  const Text(
+                    'Создания бренд',
+                    style: AppTextStyles.boldType18,
+                  ),
 
-                /// close button
-                CustomSquareIconBtn(
-                  Icons.close,
-                  size: 48,
-                  darkenColors: true,
-                  backgrounColor: AppColors.error100,
-                  iconColor: AppColors.error600,
-                  onTap: () => context.pop(),
-                ),
-              ],
-            ),
+                  /// close button
+                  CustomSquareIconBtn(
+                    Icons.close,
+                    size: 48,
+                    darkenColors: true,
+                    backgrounColor: AppColors.error100,
+                    iconColor: AppColors.error600,
+                    onTap: () => context.pop(),
+                  ),
+                ],
+              ),
 
-            /// content
-            AppUtils.kGap24,
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                /// upload image button
-                if (_imageFile.isNotNull)
-                  ClipRRect(
-                    borderRadius: AppUtils.kBorderRadius12,
-                    child: Container(
-                      color: Colors.grey,
-                      width: 150,
-                      height: 150,
-                      child: Image.file(
-                        _imageFile!,
-                        fit: BoxFit.cover,
-                        height: 150,
+              /// content
+              AppUtils.kGap24,
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  /// upload image button
+                  if (_imageFile.isNotNull)
+                    ClipRRect(
+                      borderRadius: AppUtils.kBorderRadius12,
+                      child: Container(
+                        color: Colors.grey,
                         width: 150,
+                        height: 150,
+                        child: Image.file(
+                          _imageFile!,
+                          fit: BoxFit.cover,
+                          height: 150,
+                          width: 150,
+                        ),
+                      ),
+                    )
+                  else
+                    OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        shape: const RoundedRectangleBorder(borderRadius: AppUtils.kBorderRadius12),
+                        fixedSize: const Size(150, 150),
+                        side: BorderSide(color: Colors.grey.shade300),
+                        overlayColor: Colors.grey,
+                        padding: EdgeInsets.zero,
+                      ),
+                      onPressed: _pickImage,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.image_outlined,
+                            color: context.theme.disabledColor,
+                            size: 30,
+                          ),
+                          AppUtils.kGap6,
+                          Text(
+                            'Upload image',
+                            style: AppTextStyles.rType16.copyWith(
+                              fontWeight: FontWeight.w500,
+                              color: context.theme.unselectedWidgetColor,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  )
-                else
-                  OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      shape: const RoundedRectangleBorder(borderRadius: AppUtils.kBorderRadius12),
-                      fixedSize: const Size(150, 150),
-                      side: BorderSide(color: Colors.grey.shade300),
-                      overlayColor: Colors.grey,
-                      padding: EdgeInsets.zero,
-                    ),
-                    onPressed: _pickImage,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.image_outlined,
-                          color: context.theme.disabledColor,
-                          size: 30,
-                        ),
-                        AppUtils.kGap6,
-                        Text(
-                          'Upload image',
-                          style: AppTextStyles.rType16.copyWith(
-                            fontWeight: FontWeight.w500,
-                            color: context.theme.unselectedWidgetColor,
-                          ),
-                        ),
-                      ],
-                    ),
+
+                  ///
+                  AppUtils.kGap24,
+                  AppTextField(
+                    label: 'Название',
+                    enabledBorderWith: 1,
+                    enabledBorderColor: AppColors.stroke,
+                    focusedBorderColor: AppColors.stroke,
+                    focusedBorderWith: 1,
+                    fieldController: _nameController,
                   ),
 
-                ///
-                AppUtils.kGap24,
-                AppTextField(
-                  label: 'Название',
-                  enabledBorderWith: 1,
-                  enabledBorderColor: AppColors.stroke,
-                  focusedBorderColor: AppColors.stroke,
-                  focusedBorderWith: 1,
-                  fieldController: nameController,
-                ),
-
-                /// button
-                AppUtils.kGap24,
-                BlocConsumer<BrandCubit, BrandState>(
-                  listenWhen: (p, c) => p.createBrandStatus != c.createBrandStatus,
-                  listener: (_, state) {
-                    if (state.createBrandStatus.isSuccess || state.createBrandStatus.isError) {
-                      context.pop(state.createBrandStatus.isSuccess);
-                    }
-                  },
-                  buildWhen: (p, c) => p.createBrandStatus != c.createBrandStatus,
-                  builder: (context, state) => ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColors.primary800,
-                      minimumSize: const Size.fromHeight(50),
+                  /// button
+                  AppUtils.kGap24,
+                  BlocConsumer<BrandCubit, BrandState>(
+                    listenWhen: (p, c) => p.createBrandStatus != c.createBrandStatus,
+                    listener: (_, state) {
+                      if (state.createBrandStatus.isSuccess || state.createBrandStatus.isError) {
+                        context.pop(state.createBrandStatus.isSuccess);
+                      }
+                    },
+                    buildWhen: (p, c) => p.createBrandStatus != c.createBrandStatus,
+                    builder: (context, state) => ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.primary800,
+                        minimumSize: const Size.fromHeight(50),
+                      ),
+                      onPressed: () => context.brandBloc.createBrand(
+                        name: _nameController.text,
+                        imageFile: _imageFile,
+                      ),
+                      child: state.createBrandStatus.isLoading
+                          ? const CircularProgressIndicator.adaptive(backgroundColor: Colors.white)
+                          : const Text(
+                              'Создать бренд',
+                              style: AppTextStyles.boldType16,
+                            ),
                     ),
-                    onPressed: () => context.brandBloc.createBrand(
-                      name: nameController.text,
-                      imageFile: _imageFile,
-                    ),
-                    child: state.createBrandStatus.isLoading
-                        ? const CircularProgressIndicator.adaptive(backgroundColor: Colors.white)
-                        : const Text(
-                            'Создать бренд',
-                            style: AppTextStyles.boldType16,
-                          ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
-    );
-  }
+      );
 
   Future<void> _pickImage() async {
     try {
