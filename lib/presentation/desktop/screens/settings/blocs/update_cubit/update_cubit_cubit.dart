933e388b3d511128ecbf9868ee58918f046ca737
alpjamal <mr.abdujamol1@mail.ru>
2025-10-15 11:31:47 +0500

    define const

--- lib/presentation/desktop/screens/settings/blocs/update_cubit/update_cubit_cubit.dart
+++ lib/presentation/desktop/screens/settings/blocs/update_cubit/update_cubit_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hoomo_pos/data/dtos/app_version_dto.dart';
@@ -5,21 +6,23 @@ import 'package:hoomo_pos/domain/repositories/app_version.dart';
 import 'package:injectable/injectable.dart';
 
 part 'update_cubit_state.dart';
+
 part 'update_cubit_cubit.freezed.dart';
 
 @lazySingleton
 class UpdateCubit extends Cubit<UpdateCubitState> {
-  UpdateCubit(this._appVersionRepository) : super(UpdateCubitState());
+  UpdateCubit(
+    this._appVersionRepository,
+  ) : super(const UpdateCubitState());
 
   final AppVersionRepository _appVersionRepository;
 
   Future<void> getUpdate() async {
     try {
       final res = await _appVersionRepository.getAppVersion();
-
       emit(state.copyWith(appVersion: res));
     } catch (e) {
-      print(e);
+      debugPrint('Error on UpdateCubit getUpdate: ${e.toString()}');
     }
   }
 }
