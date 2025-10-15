565d9d3aad0f07372e12b1c9783599f40b9b4434
alpjamal <mr.abdujamol1@mail.ru>
2025-10-15 17:11:54 +0500

    delete image feature

--- lib/presentation/desktop/screens/stock/screens/brands/widgets/create_brand_dialog.dart
+++ lib/presentation/desktop/screens/stock/screens/brands/widgets/create_brand_dialog.dart
@@ -6,6 +6,7 @@ import 'package:hoomo_pos/core/constants/app_utils.dart';
 import 'package:hoomo_pos/core/extensions/context.dart';
 import 'package:hoomo_pos/core/extensions/null_extension.dart';
 import 'package:hoomo_pos/core/widgets/custom_square_icon_btn.dart';
+import 'package:hoomo_pos/data/dtos/brand/brand_dto.dart';
 import 'package:image_picker/image_picker.dart';
 
 import '../../../../../../../core/styles/colors.dart';
@@ -16,20 +17,24 @@ import '../cubit/brand_cubit.dart';
 class CreateBrandDialog extends StatefulWidget {
   const CreateBrandDialog({
     super.key,
+    this.brand,
   });
+
+  final BrandDto? brand;
 
   @override
   State<CreateBrandDialog> createState() => _CreateBrandDialogState();
 }
 
 class _CreateBrandDialogState extends State<CreateBrandDialog> {
+  BrandDto? get brand => widget.brand;
   File? _imageFile;
   late final TextEditingController _nameController;
 
   @override
   void initState() {
     super.initState();
-    _nameController = TextEditingController();
+    _nameController = TextEditingController(text: brand?.name);
   }
 
   @override
@@ -85,11 +90,29 @@ class _CreateBrandDialogState extends State<CreateBrandDialog> {
                         color: Colors.grey,
                         width: 150,
                         height: 150,
-                        child: Image.file(
-                          _imageFile!,
-                          fit: BoxFit.cover,
-                          height: 150,
-                          width: 150,
+                        child: Stack(
+                          children: [
+                            Positioned.fill(
+                              child: Image.file(_imageFile!, fit: BoxFit.cover),
+                            ),
+                            if (_imageFile.isNotNull)
+                              Positioned(
+                                top: 6,
+                                right: 6,
+                                child: IconButton(
+                                  style: IconButton.styleFrom(
+                                    backgroundColor: Colors.white30,
+                                    visualDensity: VisualDensity.compact,
+                                  ),
+                                  onPressed: _delete,
+                                  icon: const Icon(
+                                    Icons.delete,
+                                    color: Colors.white,
+                                    size: 16,
+                                  ),
+                                ),
+                              ),
+                          ],
                         ),
                       ),
                     )
@@ -178,5 +201,10 @@ class _CreateBrandDialogState extends State<CreateBrandDialog> {
     } catch (e) {
       debugPrint('asklfjdasdf image pick error $e');
     }
+  }
+
+  void _delete() {
+    _imageFile = null;
+    setState(() {});
   }
 }
