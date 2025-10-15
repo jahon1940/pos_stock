5cbd9d806b1058170cde8d61bd5466aad29b0da6
alpjamal <mr.abdujamol1@mail.ru>
2025-10-15 15:56:15 +0500

    request changes

--- lib/data/dtos/brand/create_brand_request.dart
+++ lib/data/dtos/brand/create_brand_request.dart
@@ -8,7 +8,7 @@ part 'create_brand_request.g.dart';
 class CreateBrandRequest with _$CreateBrandRequest {
   factory CreateBrandRequest({
     required String cid,
-    required String organizationCid,
+    String? organizationCid,
     String? name,
     String? image,
   }) = _CreateBrandRequest;
