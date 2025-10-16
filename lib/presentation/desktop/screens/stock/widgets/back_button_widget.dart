e36c39a22501ffa7306ee4e4a3a99dd7b4c80b38
alpjamal <mr.abdujamol1@mail.ru>
2025-10-16 17:12:06 +0500

    widget changes

--- lib/presentation/desktop/screens/stock/widgets/back_button_widget.dart
+++ lib/presentation/desktop/screens/stock/widgets/back_button_widget.dart
@@ -1,25 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:hoomo_pos/core/extensions/context.dart';
 
+import '../../../../../core/constants/app_utils.dart';
+import '../../../../../core/styles/colors.dart';
+
 class BackButtonWidget extends StatelessWidget {
   const BackButtonWidget({
     super.key,
   });
 
+  static const _radius = AppUtils.kBorderRadius12;
+
   @override
   Widget build(
     BuildContext context,
   ) =>
-      ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          fixedSize: const Size.fromHeight(48),
-          visualDensity: VisualDensity.compact,
-          padding: EdgeInsets.zero,
-        ),
-        onPressed: context.pop,
-        child: const Icon(
-          Icons.arrow_back_ios_rounded,
-          color: Colors.white,
+      SizedBox(
+        height: 48,
+        width: 48,
+        child: Material(
+          borderRadius: _radius,
+          color: AppColors.primary500,
+          child: InkWell(
+            onTap: context.pop,
+            hoverColor: AppColors.primary400,
+            highlightColor: AppColors.primary300,
+            splashColor: AppColors.primary300,
+            borderRadius: _radius,
+            child: const Icon(
+              Icons.arrow_back_ios_rounded,
+              color: Colors.white,
+            ),
+          ),
         ),
       );
 }
