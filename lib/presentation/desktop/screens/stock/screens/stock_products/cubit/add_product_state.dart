ced43b157e669e04807940708f9d343de5bda2ae
alpjamal <mr.abdujamol1@mail.ru>
2025-10-16 20:51:01 +0500

    add new status

--- lib/presentation/desktop/screens/stock/screens/stock_products/cubit/add_product_state.dart
+++ lib/presentation/desktop/screens/stock/screens/stock_products/cubit/add_product_state.dart
@@ -2,8 +2,10 @@ part of 'add_product_cubit.dart';
 
 @freezed
 class AddProductState with _$AddProductState {
-  const factory AddProductState(
-      {@Default(StateStatus.initial) StateStatus status,
-      ProductDetailDto? product,
-      int? categoryId}) = _AddProductState;
+  const factory AddProductState({
+    @Default(StateStatus.initial) StateStatus status,
+    @Default(StateStatus.initial) StateStatus createProductStatus,
+    ProductDetailDto? product,
+    int? categoryId,
+  }) = _AddProductState;
 }
