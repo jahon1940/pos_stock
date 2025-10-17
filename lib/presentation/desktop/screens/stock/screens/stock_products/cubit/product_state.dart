460f71aafb139bf762a0662e308e349446131bb2
alpjamal <mr.abdujamol1@mail.ru>
2025-10-17 15:45:50 +0500

    add products parameter

--- lib/presentation/desktop/screens/stock/screens/stock_products/cubit/product_state.dart
+++ lib/presentation/desktop/screens/stock/screens/stock_products/cubit/product_state.dart
@@ -5,26 +5,32 @@ class ProductState extends Equatable {
 
   const ProductState({
     this.isUpdated = false,
-    this.product,
+    this.productPageData = const PaginatedDto(
+      results: [],
+      pageNumber: 1,
+      pageSize: 1,
+      totalPages: 1,
+      count: 1,
+    ),
     this.status = StateStatus.loading,
     this.categoryId,
     this.createProductStatus = StateStatus.loading,
   });
 
-  final ProductDetailDto? product;
+  final PaginatedDto<ProductDto> productPageData;
   final StateStatus status;
   final int? categoryId;
   final StateStatus createProductStatus;
 
   ProductState copyWith({
-    ProductDetailDto? product,
+    PaginatedDto<ProductDto>? productPageData,
     StateStatus? status,
     int? categoryId,
     StateStatus? createProductStatus,
   }) =>
       ProductState(
         isUpdated: !isUpdated,
-        product: product ?? this.product,
+        productPageData: productPageData ?? this.productPageData,
         status: status ?? this.status,
         categoryId: categoryId ?? this.categoryId,
         createProductStatus: createProductStatus ?? this.createProductStatus,
@@ -33,7 +39,7 @@ class ProductState extends Equatable {
   @override
   List<Object?> get props => [
         isUpdated,
-        product,
+        productPageData,
         status,
         categoryId,
         createProductStatus,
